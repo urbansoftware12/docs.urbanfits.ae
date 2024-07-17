2531f7f54284e453564a6f9f078a0679153f9837
Faizan <faizandevp@gmail.com>
2024-07-17 13:53:56 +0500

    took it down

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
 import "@/styles/globals.css";
 import { Inconsolata, Open_Sans } from "next/font/google";
-import MainLayout from "@/components/layout";
+// import MainLayout from "@/components/layout";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 export const incons = Inconsolata({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Urban Fits API Documentation",
-  description: "Urban Fits Developer API documentation.",
-};
+// export const metadata = {
+//   title: "Urban Fits API Documentation",
+//   description: "Urban Fits Developer API documentation.",
+// };
 
 export default function RootLayout({ children }) {
   return <html lang="en" suppressHydrationWarning>
@@ -16,9 +16,9 @@ export default function RootLayout({ children }) {
       <link rel="shortcut icon" href="./logo_black.svg" type="image/x-icon" />
     </head>
     <body className={openSans.className + " max-w-[2500px] mx-auto bg-white dark:bg-zinc-950 text-slate-800 dark:text-gray-100"}>
-      <MainLayout>
+      {/* <MainLayout>
         {children}
-      </MainLayout>
+      </MainLayout> */}
     </body>
   </html>
 }
